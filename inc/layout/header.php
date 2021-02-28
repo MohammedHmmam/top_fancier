6cb9af493040911fe9c633128a8906cb2c679073
Mohammed Hmmam <mohammedhmmam@gmail.com>
2021-02-28 20:03:34 +0200

    Add nav bar to header file

--- inc/layout/header.php
+++ inc/layout/header.php
@@ -19,3 +19,41 @@ require_once("../../initialize.php");
     <script src=<?= JS_PATH . "/popper.min.js" ?>></script>
 </head>
 <body>
+<nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="../view/index.php">Top Fancier</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+  <?php
+  
+
+  
+  ?>  
+
+
+    <ul class="nav navbar-nav navbar-right">
+      
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Create Post</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Change Password</a></li>
+          <li><a href="#">Change Email</a></li>
+          <li><a href="#">Update Nickname</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
