156dadf70b454450f04e6bccdbf9a9a6527cfd9a
Mohammed Hmmam <mohammedhmmam@gmail.com>
2021-03-01 21:20:17 +0200

    Create function to Validate value has string length

--- private/functions/validate_functions.php
+++ private/functions/validate_functions.php
@@ -1,5 +1,21 @@
 <?php
 
-
+//*Validate value has string length
+//leading and trailing space will count
+//$option: exact , max , min
+//has_length($string , ['exact'=>20]);
+//has_length($string , ['min'=>5 , 'max'=>100]);
+function has_length($value , $options =[]){
+    if(isset($options['max']) && (strlen($value) > (int)$options['max'])){
+        return false;
+    }
+    if(isset($options['min']) && (strlen($value) < (int)$options['min'])){
+        return false;
+    }
+    if(isset($options['exact']) && (strlen($value) !== (int)$options['exact'])){
+        return false;
+    }
+    return true;
+}
 
 ?>
